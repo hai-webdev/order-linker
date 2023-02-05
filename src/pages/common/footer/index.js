a7a6ec7d86aee44ca3f6765894e030931f1e9843
zb_hai <hai_web@163.com>
2023-02-05 16:56:55 +0800

    dist提交

--- src/pages/common/footer/index.js
+++
@@ -1,5 +0,0 @@
-import "./index.less";
-import "wowjs/css/libs/animate.css";
-import {WOW} from "wowjs";
-
-new WOW().init();
--- src/pages/team/index.js
+++
@@ -1,7 +0,0 @@
-import "../common/reset.css";
-// 引入页面公共部分的js
-import "../common/header";
-import "../common/footer";
-import "@/assets/global.less";
-
-import "./index.less";
